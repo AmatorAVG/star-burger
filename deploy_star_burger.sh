8b45a7f955468cba7b09497a21adc0e52f7611f5
root <root@amatoravg.example.com>
2021-12-03 14:14:45 +0300

    Add rollbar deploy script

--- deploy_star_burger.sh
+++ deploy_star_burger.sh
@@ -12,4 +12,11 @@ python manage.py makemigrations
 python manage.py migrate
 systemctl reload nginx.service
 systemctl restart starburger.service
+
+ACCESS_TOKEN=`cat .env | grep ACCESS_TOKEN= | cut -d '=' -f2`
+ENVIRONMENT=production
+LOCAL_USERNAME=root
+REVISION=`git rev-parse --verify HEAD`
+curl https://api.rollbar.com/api/1/deploy/ -F access_token=$ACCESS_TOKEN -F environment=$ENVIRONMENT -F revision=$REVISION -F local_username=$LOCAL_USERNAME
+
 echo "Deploy completed successfully"
